477c32d8b73a035f27446ba23411f8a4575200a4
ZhangLiang <59312540+XYZliang@users.noreply.github.com>
2021-03-10 22:16:20 +0800

    change https://sockboom.lol/

--- sockboom.py
+++ sockboom.py
@@ -36,7 +36,7 @@ def output(content):
 
 
 def sign(header):
-    url = 'https://sockboom.art/auth/login?email='+email+'&passwd='+passwd+''
+    url = 'https://sockboom.lol/auth/login?email='+email+'&passwd='+passwd+''
     response = requests.post(url=url, headers=header, verify=False)
     sign_message = json.loads(response.text)['msg']
     user = json.loads(response.text)['user']
@@ -55,7 +55,7 @@ def sign(header):
 
 
 def user_centre(cookie):  # 用户中心
-    url = 'https://sockboom.art/user'
+    url = 'https://sockboom.lol/user'
     headers = {
         'Cookie': cookie
     }
@@ -79,7 +79,7 @@ def user_centre(cookie):  # 用户中心
 
 
 def checkin(headers):
-    url = 'https://sockboom.art/user/checkin'
+    url = 'https://sockboom.lol/user/checkin'
     response = requests.post(url=url, headers=headers, verify=False)
     msg = json.loads(response.text)['msg']
     output('  [+]签到信息:'+msg)
